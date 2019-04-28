99408c7a6bd79410ab9cc2f332b4bec218f3bca3
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-04-28 13:00:34 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Laptop Bag", "Fashion", 10000, 2018-10-01, 2019-02-10), ("T-shirt", "Fashion", 4000, 2013-07-15, 2015-30-09),
+("Flatiron School", "Tech", 10000, 2019-03-18, 2019-07-15), ("Trip to Japan", "Travel", 4000, 2019-09-01, 2019-09-11),
+("Rent apartment", "Rent", 24000, 2019-05-01, 2025-01-01), ("Food BLog", "Blog", 1000, 2020-04-01, 2020-01-12),
+("Lear French", "Education", 2000, 2019-10-01, 2020-10-01), ("Learn Japanese", "Education", 2000, 2019-10-01, 2020-10-01),
+("Finsih Zelda", "Gaming", 50, 2019-10-01, 2019-12-20), ("Shaolin Temple", "Sports", 1000, 2019-08-01, 2025-12-30);
+
+
+INSERT INTO users (name, age)
+VALUES ("Dunia", 29), ("Shushu", 24), ("Katie", 29), ("Meru", 29), ("Meena", 29), ("Alex", 25), ("Veronika", 24), ("Tziho", 27),
+("Lena", 28), ("Mona", 26), ("Raneem", 27), ("Karam", 26), ("Lynda", 26), ("Madi", 27), ("Yassin", 25), ("Pasquale", 29),
+("Dania", 29), ("Zuzu", 26), ("Dalia", 32), ("Samara", 35);
+
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (5000, 20, 1), (1000, 19, 2), (500, 18, 3), (500, 17, 4), (100, 16, 5), (450, 16, 6), (1000, 15, 6), (700, 14, 7), (150, 13, 8),
+(3000, 12, 9), (200, 11, 10), (750, 10, 1), (800, 9, 2), (1500, 8, 3), (500, 7, 4), (2000, 6, 5), (1000, 5, 6), (450, 4, 7),
+(700, 3, 8), (2000, 2, 9), (150, 1, 10), (300, 5, 5), (300, 7 ,8), (450, 16, 5), (800, 18, 4), (980, 12, 10), (600, 18, 4),
+(200, 10, 2), (500, 8, 2), (150, 6, 8);
